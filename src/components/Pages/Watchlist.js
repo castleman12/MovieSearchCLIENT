418f7b27231293c2c02cb382e8edd4c7c01ec636
castleman12 <68916886+castleman12@users.noreply.github.com>
2020-11-24 14:47:31 -0600

    added more to home, searchresults, and watchlist

---
+++ src/components/Pages/Watchlist.js
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+import {Header, Navbar} from 'reactstrap';
+
+const Watchlist = () => {
+
+
+return (
+    <Header>
+        <Navbar>
+            <div>Sign Up/Log In</div>
+            <div>MegaMovies</div>
+            <div>Watchlist</div>
+        </Navbar>
+     </Header>
+)
+
+}
