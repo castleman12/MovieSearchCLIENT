5e64ccf560e51904ed0bdc7db4425fa9f2e5c19b
castleman12 <68916886+castleman12@users.noreply.github.com>
2020-11-28 13:54:57 -0600

    attempted more info

--- src/components/Pages/SearchResults.js
+++ src/components/Pages/SearchResults.js
@@ -1,10 +1,11 @@
 import {useState, useEffect} from 'react';
-import {Button} from 'reactstrap';
+import {Button, Form} from 'reactstrap';
 import './SearchResults.css';
 
 const SearchResults = (props) => {
 
     const [searchResults, setSearchResults] = useState([]);
+   
 
     useEffect(() => {
         async function fetchResults(){
@@ -33,6 +34,26 @@ export default SearchResults;
 
 const ShowData = (props) => {
 
+
+  const MoreInfo = (id) => {
+        // const [moreInfo, setMoreInfo] = useState([])
+        const [imdbID, setimdbID ] = useState([])
+        useEffect(() => {  
+                fetch(`https://api.themoviedb.org/3/movie/${id}/external_ids?api_key=82b354b312b56da6907439cf056a2d21`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+                
+                    })
+                    .then(res => res.json())
+                    .then(data => setimdbID(data))
+                
+            
+            }, [])
+            console.log(imdbID)
+    }
+
     return (
         props.searchResults.map((movie, index) => {
         return(
@@ -43,9 +64,49 @@ const ShowData = (props) => {
             <p><b>About the film:</b> <br/>
                 {movie.overview}</p>
             <br/>
+            
+           <Button type="submit" id="add" onClick={() => {MoreInfo(movie.id)}}>More Info</Button>
             <Button id="add">Add to Watchlist!</Button>
             </div>
         )
         })
     )
-}
+    
+}  
+
+// const MoreInfo = (id) => {
+
+// // const [moreInfo, setMoreInfo] = useState([])
+
+// const [imdbID, setimdbID ] = useState([])
+
+// useEffect(() => {
+
+//        fetch(`https://api.themoviedb.org/3/movie/${id}/external_ids?api_key=82b354b312b56da6907439cf056a2d21`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//              }
+//         })
+//         .then(res => res.json())
+//         .then(data => setimdbID(data.imdb_id))
+//         .then(console.log(imdbID))
+//     }
+   
+   
+//     , [])
+// }
+
+// useEffect(() => {
+//     async function fetchResults(){
+//         let response = await fetch(`http://www.omdbapi.com/?apikey= 2fd2161a&`, {
+//         method: 'GET'
+//         })
+//         response = await response.json()
+//         setSearchResults(response.results)
+//     }
+//     fetchResults()
+//     }, [])
+    
+// }
+
